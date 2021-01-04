f0affc6300a2e6c6cbe9027fac92189811e3b204
subhroy <subhasis.it@gmail.com>
2021-01-04 20:55:08 +0530

    Stream : More example

--- src/main/java/stream/practise/MyStreamPractise2.java
+++ src/main/java/stream/practise/MyStreamPractise2.java
@@ -15,52 +15,53 @@ public class MyStreamPractise2 {
         nameList.add("kamesh");
         nameList.add("vittal");
         nameList.add("rajithravindran");
-        System.out.println(nameList);
+        System.out.println("Original nameList : "+ nameList);
 
-        List resultLst = nameList.stream().filter(s -> s.length()>6).collect(Collectors.toList());
-        System.out.println(resultLst);
+        List resultLst = nameList.stream().filter(s -> s.length()>7).collect(Collectors.toList());
+        System.out.println("resultLst(length > 7) : "+resultLst);
 
         List resultLst2 = nameList.stream().map(s -> s.length()).collect(Collectors.toList());
-        System.out.println(resultLst2);
+        System.out.println("Print Length of each name in nameList : "+resultLst2);
 
         List resultLst3 = nameList.stream().map(s->s.toUpperCase()).collect(Collectors.toList());
-        System.out.println(resultLst3);
+        System.out.println("List with Upper Case change : "+resultLst3);
 
         long noOfTotalStreamElement = nameList.stream().count();
-        System.out.println(noOfTotalStreamElement);
+        System.out.println("Total No of element in nameList : "+noOfTotalStreamElement);
 
         long noOfElement7 = nameList.stream().filter(s->s.length()>6).count();
-        System.out.println(noOfElement7);
+        System.out.println("Total No of element( > 7) in nameList : " + noOfElement7);
 
 
+        System.out.println("\n\n//////// ANOTHER EXAMPLE /////////////\n\n");
         ArrayList<Integer> numberList = new ArrayList<Integer>();
         numberList.add(15);
         numberList.add(5);
         numberList.add(25);
         numberList.add(10);
         numberList.add(20);
-        System.out.println(numberList);
+        System.out.println("Original numberList : " + numberList);
 
         //Default sorting by sorted() method of Stream
         List<Integer> sortedLst = numberList.stream().sorted().collect(Collectors.toList());
-        System.out.println(sortedLst);
+        System.out.println("Sorted(Default) numberList : " + sortedLst);
 
         //Custom sorting by Stream
         List<Integer> customSortedLst = numberList.stream().sorted((i1,i2)-> -i1.compareTo(i2)).collect(Collectors.toList());
-        System.out.println(customSortedLst);
+        System.out.println("Sorted(Custom) numberList : " + customSortedLst);
 
 
         //Find min and max value inside Stream
         Integer minValue = numberList.stream().min((i1,i2)->i1.compareTo(i2)).get();
-        System.out.println(minValue);
+        System.out.println("minValue in numberList : " + minValue);
 
         Integer maxValue = numberList.stream().max((i1,i2)->i1.compareTo(i2)).get();
-        System.out.println(maxValue);
+        System.out.println("maxValue in numberList : " + maxValue);
 
         //forEach of Stream
         nameList.stream().forEach(s -> System.out.println(s));
 
-        System.out.println("\n -----try printing using method reference ---- \n");
+        System.out.println("\n -----Try printing using Method Reference ---- \n");
 
         //forEach of Stream using method reference
         nameList.stream().forEach(System.out::println);
@@ -76,14 +77,15 @@ public class MyStreamPractise2 {
         //Stream.of on Group of values.
         Stream<Integer> st =Stream.of(3,33,333,3333,33333); // creating the stream object here..
         //now you can call any method of stream on this
-        System.out.println("print each element");
+        System.out.println("Print each element");
         st.forEach(System.out::println);
 
 
         //Creating a double array and convert it to stream and using that stream
         Double[] myArr = {10.1,10.2,10.3,10.4,10.5};
-        Stream<Double> mystream = Stream.of(myArr);
-        mystream.forEach(System.out::println);
+        System.out.println("Print each element from a Double Array");
+        Stream<Double> myStream = Stream.of(myArr);
+        myStream.forEach(System.out::println);
 
     }
 }
